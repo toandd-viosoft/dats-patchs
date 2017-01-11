3d82de79919e469ea913e8f39a08dcb411c7bc76
Dao Dang Toan <toandd@viosoft.com>
2017-01-11 15:21:52 +0700

    Get users's information before getting source
    To make sure that users have a permission to get source
    Adding a script to automatically get source from github repo
    By using expect module
    
    Signed-off-by: Dao Dang Toan <toandd@viosoft.com>

--- common-scripts/pre-run.sh
+++ common-scripts/pre-run.sh
@@ -10,53 +10,30 @@ sudo sed -i -e 's/GSSAPIAuthentication\ yes/GSSAPIAuthentication\ no/g' /etc/ssh
 
 sudo service sshd restart
 
-#rm -rf /root/.ssh/id*
-#rm -rf /root/.ssh/known_hosts
-cat - >genkey.sh <<'EOF'
-#!/usr/bin/expect
-spawn ssh-keygen
-expect "Enter file in which to save the key (/root/.ssh/id_rsa): "
-send -- "\r"
-expect "Enter passphrase (empty for no passphrase): "
-send -- "\r"
-expect "Enter same passphrase again: "
-send -- "\r"
-sleep 2
+[ -d /opt/nfv-mano ] || mkdir -p /opt/nfv-mano
+cd /opt/nfv-mano
+# Removing the old source
+rm -rf validium.io
+# Creating file which contains users's information
+cat - >users_information.sh <<'EOF'
+export GIT_USERNAME=
+export GIT_PASSWD=
 EOF
-chmod +x genkey.sh
-./genkey.sh
 
-cat - >copy_keygen.sh <<'EOF'
+source /opt/nfv-mano/users_information.sh
+
+cat - >get_source.sh <<'EOF'
 #!/usr/bin/expect
-set host [lrange $argv 0 0]
-
-set username [lrange $argv 1 1]
-set password [lrange $argv 2 2]
-
-set timeout 10
-spawn /usr/bin/ssh-copy-id -i /root/.ssh/id_rsa.pub $username@$host
-match_max 100000
-expect "*?(yes/no)\?"
-send -- "yes\r"
-# Look for password prompt
-expect {
-"*?assword:*" { send -- "$password\r"; send -- "\r"; exp_continue }
-eof { exit 1 }
-"Now try*\r" { exit 0 }
-timeout { exit 1 }
-}
-exit 0
+# Users's information
+set username [lrange $argv 0 0]
+set password [lrange $argv 1 1]
+# Get source code
+spawn git clone https://github.com/viosoft-corp/validium.io.git
+expect "Username for 'https://github.com': "
+send -- "$username\r"
+expect "*?github.com': "
+send -- "$password\r"
+sleep 5
 EOF
-chmod +x copy_keygen.sh
-./copy_keygen.sh 10.64.0.105 root validium2016
-sleep 2
-./copy_keygen.sh 10.64.0.106 root validium2016
-sleep 2
-./copy_keygen.sh 10.64.0.101 root validium2016
-sleep 3
-cd /root
-scp -r root@10.64.0.101:/root/validium.io .
-cd /root/validium.io/nfv-mano/cloudify-crucio/crucio-scripts/CRUCIO-SYSTEM-SETUP/host-setup
-cd setup_host_environment/
-./setup_host_environment.sh
-
+chmod +x get_source.sh
+./get_source.sh $GIT_USERNAME $GIT_PASSWD
