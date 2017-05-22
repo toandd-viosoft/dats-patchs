3efaab154393493d97ec5d841d8a233f25f913ae
Dao Dang Toan <toandd@viosoft.com>
2017-05-22 13:50:04 +0700

    Ssh without enter password
    Modify config file before ssh
    
    Signed-off-by: Dao Dang Toan <toandd@viosoft.com>

--- scripts/one_vm_scripts/sut/ubuntu-server-cloudimg-dpdk-modify.sh
+++ scripts/one_vm_scripts/sut/ubuntu-server-cloudimg-dpdk-modify.sh
@@ -1,122 +1,132 @@
-#!/bin/bash
+#!/bin/sh
+##############################################################################
+# Copyright (c) 2015 Ericsson AB and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
 
-set -x
+# installs required packages
+# must be run from inside the image (either chrooted or running)
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+set -ex
+# Get some VMs's parameters
+# source ./vm-args.sh
 
-# Commandline arguments
-PKT_SIZE=$1             # packet size
-TESTS=$2                # 01_port_fwd
-TOLERATED_LOSS=$3       # 0.0
-LOGGING_LEVEL=$4        # INFO
-TEST_DURATION=$5        # 5s
-#USER=$6                # root
-#PASSWORD=$7            # validium2016
-PCI_TYPE=$6             # 82599ES
-PCI_PORT_NUMBER=$7      # 4
-TESTER_IP_ADDRESS=$8    # host ip address
-SUT_IP_ADDRESS=$9       # target ip address
+if [ $# -eq 1 ]; then
+    nameserver_ip=$1
 
-DPDK_DRIVER=igb_uio
-RTE_SDK=/opt/crucio/dpdk
-RTE_TARGET=x86_64-native-linuxapp-gcc
-RTE_BIND=$RTE_SDK/tools/dpdk_nic_bind.py
-PROX_DIR=/opt/crucio/PROX
-USER_NAME=root
-PASSWD="validium2016"
-
-create_log_file()
-{
-cat << EOF > /opt/crucio/result.log
-1
-EOF
-}
-
-change_result()
-{
-    echo 0 > /opt/crucio/result.log
-}
-
-load_modules()
-{
-    if lsmod | grep "uio" &> /dev/null ; then
-    echo "uio module is loaded"
-    else
-    modprobe uio
-    fi
-
-    if lsmod | grep "igb_uio" &> /dev/null ; then
-    echo "igb_uio module is loaded"
-    else
-    insmod $RTE_SDK/x86_64-native-linuxapp-gcc/kmod/igb_uio.ko
-    fi
-
-    if lsmod | grep "rte_kni" &> /dev/null ; then
-    echo "rte_kni module is loaded"
-    else
-    insmod $RTE_SDK/x86_64-native-linuxapp-gcc/kmod/rte_kni.ko
-    fi
-}
-
-change_permissions()
-{
-    # chmod 777 /sys/bus/pci/drivers/virtio-pci/*
-    chmod 777 /sys/bus/pci/drivers/igb_uio/*
-}
-
-add_interface_to_dpdk(){
-path="$(pwd)"
-
-# Get the name of NIC card, which can connect to internet
-NET=$(route | grep '^default' | grep -o '[^ ]*$')
-ipaddr=$(ifconfig $NET  | grep -ie "inet" | grep -ioEe "([0-9]+\.){3}[0-9]+")
-ipaddr=($ipaddr)
-IP=${ipaddr[0]}
-#Shouldn't bind the NIC card which was being used to access internet, get its pci address
-black_pci=(`ethtool -i $NET | grep bus-info | cut -d':' -f3-5`)
-#black_pci=(`$RTE_SDK/tools/dpdk_nic_bind.py -s | grep Active |cut -d':' -f2-4`)
-pci_port_type=(`lspci | grep Ether | grep $PCI_TYPE | cut -d' ' -f1`)
-num_pci_ports=${#pci_port_type[@]}
-
-#Binding nic cards to dpdk driver, checking whether server has enough pci ports or not
-if [ $num_pci_ports -ge $PCI_PORT_NUMBER ]
-then
-    echo "Using igb_uio driver"
-cd $path
-
-  if [ $DPDK_DRIVER = igb_uio ]
-  then
-    load_modules
-    change_permissions
-    port=0
-    while [ $port -lt $PCI_PORT_NUMBER ]
-    do
-        $RTE_BIND -u ${pci_port_type[$port]}
-        $RTE_BIND -b igb_uio ${pci_port_type[$port]}
-        port=`expr $port + 1`
-    done
-    $RTE_BIND --status
-  elif [ $DPDK_DRIVER = "vfio-pci" ]
-  then
-    echo "Using vfio-pci driver"
-    lsmod |grep -w "^uio" >/dev/null 2>&1 || modprobe vfio-pci
-    port=0
-    while [ $port -lt $PCI_PORT_NUMBER ]
-    do
-        $RTE_BIND -u ${pci_port_type[$port]}
-        $RTE_BIND -b vfio-pci ${pci_port_type[$port]}
-        port=`expr $port + 1`
-    done
-    $RTE_BIND --status
-  else
-    echo "This driver is not supported by validium"
-  fi
-else
-    echo "The number of ports is greater than real number of ports"
+    # /etc/resolv.conf is a symbolic link to /run, restore at end
+    /bin/rm -f /etc/resolv.conf
+    #/usr/bin/touch /etc/resolv.conf
+    echo "nameserver $nameserver_ip" > /etc/resolv.conf
+    echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+    echo "nameserver 8.8.4.4" >> /etc/resolv.conf
 fi
-}
 
-create_ssh_copykey()
-{
-cd /opt
+# iperf3 only available for wily in backports
+#grep wily /etc/apt/sources.list && \
+#    echo "deb http://archive.ubuntu.com/ubuntu/ wily-backports main restricted universe multiverse" >> /etc/apt/sources.list
+
+# Workaround for building on CentOS (apt-get is not working with http sources)
+# sed -i 's/http/ftp/' /etc/apt/sources.list
+
+# Force apt to use ipv4 due to build problems on LF POD.
+echo 'Acquire::ForceIPv4 "true";' > /etc/apt/apt.conf.d/99force-ipv4
+
+sed -i '/^.*GRUB_CMDLINE_LINUX=.*$/d' /etc/default/grub
+echo "GRUB_CMDLINE_LINUX=\"resume=/dev/sda1 default_hugepagesz=$VM_DEF_HUG hugepagesz=$VM_HUGZ hugepages=$VM_HUG isolcpus=$VM_ISOL_CPU rcu_nocbs=$VM_RCU_NOCBS intel_idle.max_cstate=$VM_IDLE_MAX_CSTATE processor.max_cstate=$VM_P_MAX_CSTATE\"" >> /etc/default/grub
+#echo 'vm.nr_hugepages=8912' >> /etc/sysctl.conf
+#echo 'huge /mnt/huge hugetlbfs defaults 0 0' >> vi /etc/fstab
+
+/bin/mkdir /mnt/huge
+/bin/chmod 777 /mnt/huge
+
+for i in {1..2}
+do
+    /usr/bin/touch /etc/network/interfaces.d/eth$i.cfg
+    /bin/chmod 777 /etc/network/interfaces.d/eth$i.cfg
+    echo "auto eth$i" >> /etc/network/interfaces.d/eth$i.cfg
+    echo "iface eth$i inet dhcp" >> /etc/network/interfaces.d/eth$i.cfg
+done
+
+# this needs for checking dpdk status, adding interfaces to dpdk, bind, unbind etc..
+
+# Add hostname to /etc/hosts.
+# Allow console access via pwd
+/usr/bin/touch /etc/cloud/cloud.cfg.d/10_etc_hosts.cfg
+
+echo "manage_etc_hosts: True" > /etc/cloud/cloud.cfg.d/10_etc_hosts.cfg
+echo "password: validium2016" >> /etc/cloud/cloud.cfg.d/10_etc_hosts.cfg
+echo "chpasswd: { expire: False }" >> /etc/cloud/cloud.cfg.d/10_etc_hosts.cfg
+echo "ssh_pwauth: True" >> /etc/cloud/cloud.cfg.d/10_etc_hosts.cfg
+
+
+linuxheadersversion=`echo ls boot/vmlinuz* | cut -d- -f2-`
+sudo locale-gen "en_US.UTF-8"
+sudo dpkg-reconfigure locales
+apt-get update
+apt-get install -y \
+    fio \
+    gcc \
+    git \
+    expect\
+    net-tools\
+    linux-tools-common \
+    linux-tools-generic \
+    lmbench \
+    make \
+    netperf \
+    patch \
+    perl \
+    rt-tests \
+    stress \
+    sysstat \
+    linux-headers-$linuxheadersversion \
+    libpcap-dev \
+    lua5.2
+
+ifconfig -a
+
+echo "$USERNAME:$PASSWD"  | chpasswd
+cat /etc/network/interfaces
+echo "auto lo eth0" > /etc/network/interfaces
+echo "iface lo inet static" >> /etc/network/interfaces
+echo "auto eth0" >> /etc/network/interfaces
+echo "iface eth0 inet static" >> /etc/network/interfaces
+echo "address $VM1_IP" >> /etc/network/interfaces
+echo "netmask $VM_NETMASK" >> /etc/network/interfaces
+echo "gatway $GW" >> /etc/network/interfaces
+cat /etc/network/interfaces
+
+# SSH setting
+sudo sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+# Allow ssh using password and don't ask (yes/no) at the first time connect to
+sudo sed -i -e 's/GSSAPIAuthentication\ yes/GSSAPIAuthentication\ no/g' /etc/ssh/sshd_config
+sudo sed -i -e 's/PasswordAuthentication\ no/PasswordAuthentication\ yes/g' /etc/ssh/sshd_config
+sudo sed -i '/GSSAPIAuthentication\ yes/a \\tStrictHostKeyChecking\ no' /etc/ssh/ssh_config
+sudo sed -i -e 's/GSSAPIAuthentication\ yes/GSSAPIAuthentication\ no/g' /etc/ssh/ssh_config
+# Allow sudo from ssh
+echo 'Defaults !requiretty' >/etc/sudoers.d/ssh-sudo-without-tty
+
+# Generate SSH key pair
+cd /root
+cat - >genkey.sh <<'EOF'
+#!/usr/bin/expect
+spawn ssh-keygen
+expect "Enter file in which to save the key (/root/.ssh/id_rsa): "
+send -- "\r"
+expect "Enter passphrase (empty for no passphrase): "
+send -- "\r"
+expect "Enter same passphrase again: "
+send -- "\r"
+sleep 2
+EOF
+chmod +x genkey.sh
+#./genkey.sh
+# The scripts with 3 parameters
 cat - >copy_keygen.sh <<'EOF'
 #!/usr/bin/expect
 set host [lrange $argv 0 0]
@@ -137,143 +147,161 @@ timeout { exit 1 }
 exit 0
 EOF
 chmod +x copy_keygen.sh
+#Then restart SSH:
+service ssh restart
+# SELINUX is disabled by Ubuntu default
+# Disable firewall, irqbalance and randomize memory
+sudo ufw disable
+sed -i 's/ENABLED="1"/ENABLED="0"/g' /etc/default/irqbalance
+echo "kernel.randomize_va_space = 0" >>  /etc/sysctl.conf
+# The Linux kernel is updated to 4.4.x because it’s NTT Docomo requirement (will be supported later)
+#sudo apt-get install linux-image-generic-lts-xenial
+#sudo apt-get install linux-headers-generic-lts-xenial
+# use bash shell as default shell
+ln -sf /bin/bash /bin/sh
+sudo apt-get -y install yum-utils wget vim tcpdump screen
+sudo apt-get -y install sed gawk grep wget tar gzip bzip2 unzip coreutils pciutils psmisc
+sudo apt-get -y install gcc make git autoconf automake libtool python perl patch
+sudo apt-get -y install net-tools
+sudo apt-get -y install grub-common
+ln -s /usr/bin/env /bin/env
+apt-get -y install liblua5.2-dev
+apt-get -y install libpcap-dev
+apt-get -y install libncurses5-dev libncursesw5-dev
+apt-get -y install libedit-dev
+sudo sh -c 'echo "deb http://archive.canonical.com/ubuntu/ vivid partner" >> /etc/apt/sources.list.d/canonical_partner.list'
+apt-get -y update
+# Cannot install two packages below, don't know the reason, So will install it later when VMs booted
+# apt-get -y install linux-generic linux-headers-generic
+# apt-get -y install linux-generic linux-headers-generic
+# Setup working environment variables
+export RTE_SDK=/root/dpdk
+export RTE_TARGET=x86_64-native-linuxapp-gcc
+export RTE_BIND=$RTE_SDK/tools/dpdk_nic_bind.py
 
-}
+cd /root
+mkdir -p ~/white
+# Get patch
+git clone https://github.com/toandd-viosoft/dats-patchs.git
+cp -r dats-patchs/common-patchs/patch ~/white
+# Get dpdk follow version
+wget http://www.dpdk.org/browse/dpdk/snapshot/dpdk-$DPDK_VER.tar.gz
+tar -zxf dpdk-$DPDK_VER.tar.gz
+ln -snf dpdk-$DPDK_VER dpdk
+if [ $DPDK_VER = 2.2.0 ]
+then
+# Apply patch for dpdk 2.2.0
+echo "Applying patch for Bng-QoS test"
+cd /root/dpdk
+patch -p1 < /root/white/patch/dpdk/virtio-always-run-mbuf-allocation-loop.patch
+cat > config/defconfig_$RTE_TARGET << EOL
+#include "common_linuxapp"
 
-copy_sshkey_to_sut()
-{
-cd /opt
-[ -f copy_keygen.sh ] || create_ssh_copykey
-cd /opt
-./copy_keygen.sh $SUT_IP_ADDRESS $USER_NAME $PASSWD
-sleep 2
+# OVS-specific customization
+CONFIG_RTE_BUILD_COMBINE_LIBS=y
+CONFIG_RTE_LIBRTE_VHOST=y
+CONFIG_RTE_LIBRTE_VHOST_USER=y
+#
+CONFIG_RTE_ARCH_64=y
+CONFIG_RTE_TOOLCHAIN="gcc"
+CONFIG_RTE_TOOLCHAIN_GCC=y
+CONFIG_RTE_MACHINE="native"
+CONFIG_RTE_ARCH="x86_64"
+CONFIG_RTE_ARCH_X86_64=y
+EOL
+fi
+#cd $RTE_SDK
+#make install T=$RTE_TARGET
 
-}
-
-create_dats_config_file()
-{
-cat << EOF > /opt/crucio/DATS/dats.cfg
-;
-; Dataplane Automated Testing System
-;
-; Copyright (c) 2015-2016, Intel Corporation.
-; All rights reserved.
-;
-; Redistribution and use in source and binary forms, with or without
-; modification, are permitted provided that the following conditions
-; are met:
-;
-;   * Redistributions of source code must retain the above copyright
-;     notice, this list of conditions and the following disclaimer.
-;   * Redistributions in binary form must reproduce the above copyright
-;     notice, this list of conditions and the following disclaimer in
-;     the documentation and/or other materials provided with the
-;     distribution.
-;   * Neither the name of Intel Corporation nor the names of its
-;     contributors may be used to endorse or promote products derived
-;     from this software without specific prior written permission.
-;
-; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-; A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-; OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-; SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-; LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-; DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-; THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-; OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-;
-[general]
-; Which packet sizes to use when testing. Tests are run for each
-; packet size in turn.
-; Default value: 64,128,256,512,1024,1280,1518
-pkt_sizes=$PKT_SIZE
-
-; Which tests to run. This can be overridden by specifying test names
-; on the command line.
-; Default value: all tests in the tests/ directory.
-tests=$TESTS
-
-; This setting specifies the percentage of packets sent by the Tester that can
-; be tolerated to be lost, instead of being properly forwarded by the SUT.
-; Note: Although the % symbol must not be provided here, the supplied value is a
-; percentage: a value of 0.001 means 0.001%, that is 1 packet is tolerated to be
-; lost out of 100,000 packets sent by the Tester to the SUT.
-; Default value: 0.0
-tolerated_loss = $TOLERATED_LOSS
-
-
-[logging]
-; Valid values are DEBUG, INFO, WARNING, ERROR, CRITICAL.
-level=$LOGGING_LEVEL
-
-; Default value: dats.log
-;file=$LOG_FILE
-
-; See https://docs.python.org/2/library/logging.html#logging.Formatter for an
-; explanation on the log format and datefmt and
-; https://docs.python.org/2/library/logging.html#logrecord-attributes for a list
-; of format attributes.
-;format=%(asctime)-15s %(levelname)-8s %(filename)20s:%(lineno)-3d %(message)s
-;datefmt=
-
-; 0 to append to logfile if it exists already, 1 to overwrite.
-;overwrite=$LOG_OVERWRITE
-
-
-[tester]
-ip=$TESTER_IP_ADDRESS
-user=$USER_NAME
-
-; Default value: /root/dpdk
-rte_sdk=$RTE_SDK
-
-; Default value: x86_64-native-linuxapp-gcc
-;rte_target=$RTE_TARGET
-
-; Default value: /root/prox
-prox_dir=$PROX_DIR
-
-
-[sut]
-ip=$SUT_IP_ADDRESS
-user=$USER_NAME
-
-; Default value: /root/dpdk
-rte_sdk=/root/dpdk
-
-; Default value: x86_64-native-linuxapp-gcc
-;rte_target=$RTE_TARGET
-
-; Default value: /root/prox
-prox_dir=/root/PROX
+# Install PROX (PROX v031)
+cd /root
+git clone https://github.com/nvf-crucio/PROX
+cd PROX
+git checkout $PROX_VER
+if [ $PROX_VER = 85806f9431cc2d70ad5a82d3d07eff78af3486ea ]
+then
+echo "Applying patch for BNG-QoS test"
+cp /root/white/patch/prox/* ../
+patch -p1 < ../more-rxtx-desc-and-larger-mbuf-cache.patch
+fi
+# Create ENV setup script, meant to be sourced rather than executed
+cd /root/white
+cat - >env.sh <<'EOF'
+export RTE_SDK=/root/dpdk
+export RTE_TARGET=x86_64-native-linuxapp-gcc
+export RTE_BIND=$RTE_SDK/tools/dpdk_nic_bind.py
+cd ~
 EOF
-}
 
-run_crucio()
-{
-    blacklist=$(lspci |grep Eth |awk '{print $1}'|head -1)
-    cd /opt/crucio/DATS && python dats.py -f dats.cfg -r validium.out
-}
+cat - >ports-to-bind <<EOF
+00:04.0
+00:05.0
+00:06.0
+00:07.0
+EOF
 
-convert_result_to_json()
-{
-    # pip install BeautifulSoup4
-    python ~/crucio_convert_to_json.py /opt/crucio/DATS/validium.out/summary.html
-}
+cat - >00-after-boot.sh <<'EOF'
+#!/bin/sh
+lsmod |grep -w "^uio" >/dev/null 2>&1 || modprobe uio
+lsmod |grep -w "^igb_uio" >/dev/null 2>&1 || insmod $RTE_SDK/$RTE_TARGET/kmod/igb_uio.ko
+bound=$($RTE_BIND --status |sed -n -e '/drv=igb_uio/ s/^\([[:xdigit:]:.]*\).*$/\1/p')
+[ -z "$bound" ] || $RTE_BIND -u $bound
+$RTE_BIND -b igb_uio $(cat ports-to-bind)
+$RTE_BIND --status
+EOF
 
-main()
-{
-    create_log_file
-    create_dats_config_file
-    copy_sshkey_to_sut
-    add_interface_to_dpdk
-    run_crucio
-    convert_result_to_json
-    change_result
-}
+cat - >01-mount-hugepage.sh <<'EOF'
+#!/bin/sh
+mkdir -p /mnt/huge
+umount `awk '/hugetlbfs/ { print $2 }' /proc/mounts` >/dev/null 2>&1
+echo 4096 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+#echo 512 > /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
+#echo 1 > /sys/devices/system/node/node0/hugepages/hugepages-1048576kB/nr_hugepages
+#echo 1 > /sys/devices/system/node/node1/hugepages/hugepages-1048576kB/nr_hugepages
+mount -t hugetlbfs nodev /mnt/huge
+EOF
 
-main
+chmod +x 00-after-boot.sh
+chmod +x 01-mount-hugepage.sh
+
+# Run all after VM booting
+cd /root
+cat - >run-all-after-vm-booting.sh <<'EOF'
+#!/bin/sh
+#apt-get update
+EOF
+
+chmod +x run-all-after-vm-booting.sh
+#==========================================================================
+echo "route add default gw $GW" >> /root/run-all-after-vm-booting.sh
+echo "apt-get update" >> /root/run-all-after-vm-booting.sh
+echo "apt-get install expect" >> /root/run-all-after-vm-booting.sh
+echo "apt-get -y install linux-generic linux-headers-generic --force-yes" >> /root/run-all-after-vm-booting.sh
+echo "source ~/white/env.sh" >> /root/run-all-after-vm-booting.sh
+echo "cd $RTE_SDK" >> /root/run-all-after-vm-booting.sh
+echo "make install T=$RTE_TARGET" >> /root/run-all-after-vm-booting.sh
+echo "cd /root/PROX" >> /root/run-all-after-vm-booting.sh
+echo "make" >> /root/run-all-after-vm-booting.sh
+echo "cd ~/white" >> /root/run-all-after-vm-booting.sh
+echo "./00-after-boot.sh" >> /root/run-all-after-vm-booting.sh
+echo "./01-mount-hugepage.sh" >> /root/run-all-after-vm-booting.sh
+echo "./genkey.sh" >> /root/run-all-after-vm-booting.sh
+echo "sleep 2" >> /root/run-all-after-vm-booting.sh
+echo "cd /root" >> /root/run-all-after-vm-booting.sh
+echo "./copy_keygen.sh $TG_IP $USERNAME $PASSWD" >> /root/run-all-after-vm-booting.sh
+echo "sleep 2" >> /root/run-all-after-vm-booting.sh
+echo "./copy_keygen.sh $SUT_IP $USERNAME $PASSWD" >> /root/run-all-after-vm-booting.sh
+echo "sleep 2" >> /root/run-all-after-vm-booting.sh
+echo "./copy_keygen.sh 127.0.0.1 $USERNAME $PASSWD" >> /root/run-all-after-vm-booting.sh
+echo "sleep 2" >> /root/run-all-after-vm-booting.sh
+#==========================================================================
+#git clone https://github.com/beefyamoeba5/ramspeed.git /opt/tempT/RAMspeed
+#cd /opt/tempT/RAMspeed/ramspeed-2.6.0
+#/bin/mkdir temp
+#bash build.sh
+
+#git clone https://github.com/beefyamoeba5/cachestat.git /opt/tempT/Cachestat
+
+# restore symlink
+# /bin/ln -sf /run/resolvconf/resolv.conf /etc/resolv.conf
 
