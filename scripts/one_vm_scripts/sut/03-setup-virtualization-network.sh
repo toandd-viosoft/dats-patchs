599ab5c1158b3e959d36cadfd1b31445b05c2b80
Dao Dang Toan <toandd@viosoft.com>
2017-05-25 11:10:04 +0700

    Get correct management interface
    Make sure management interface different to management bridge
    
    Signed-off-by: Dao Dang Toan <toandd@viosoft.com>

--- scripts/one_vm_scripts/sut/03-setup-virtualization-network.sh
+++ scripts/one_vm_scripts/sut/03-setup-virtualization-network.sh
@@ -17,6 +17,27 @@ virsh net-destroy default
 [ -z "$MGMT_IF" ] && MGMT_IF=$($RTE_BIND --status |sed -n -e '/\*Active\*/ s/^.* if=\([^ ]*\) .*$/\1/p')
 [ $(echo "$MGMT_IF" |wc -w) -eq 1 ] && echo "OK, management interface is: $MGMT_IF" || echo "Please manually set MGMT_IF"
 
+if [ $MGMT_IF == $MGMT_BR ]
+then
+    # Move DNS configuration from management interface to network
+    sed -n -e '/^DNS/ p' /etc/sysconfig/network-scripts/ifcfg-$MGMT_IF >>/etc/sysconfig/network
+    sed -i -e '/^DNS/ d' /etc/sysconfig/network-scripts/ifcfg-$MGMT_IF
+    sed -n -e '/^DOMAIN/ p' /etc/sysconfig/network-scripts/ifcfg-$MGMT_IF >>/etc/sysconfig/network
+    sed -i -e '/^DOMAIN/ d' /etc/sysconfig/network-scripts/ifcfg-$MGMT_IF
+
+    # Create management bridge with management interface attached
+    # Immediately restart network service to avoid losing remote connection
+    # Note: It might take one minute for network service to restart
+    virsh iface-unbridge $MGMT_BR
+    virsh iface-bridge $MGMT_IF $MGMT_BR ; service network restart
+    virsh net-autostart default --disable
+    virsh net-destroy default
+    # Determine management interface name
+    # If this fails, manually set MGMT_IF
+    [ -z "$MGMT_IF" ] && MGMT_IF=$($RTE_BIND --status |sed -n -e '/\*Active\*/ s/^.* if=\([^ ]*\) .*$/\1/p')
+    [ $(echo "$MGMT_IF" |wc -w) -eq 1 ] && echo "OK, management interface is: $MGMT_IF" || echo "Please manually set MGMT_IF"
+fi
+
 # Move DNS configuration from management interface to network
 sed -n -e '/^DNS/ p' /etc/sysconfig/network-scripts/ifcfg-$MGMT_IF >>/etc/sysconfig/network
 sed -i -e '/^DNS/ d' /etc/sysconfig/network-scripts/ifcfg-$MGMT_IF
