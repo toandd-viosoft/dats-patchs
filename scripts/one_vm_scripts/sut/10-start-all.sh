4ac8ee01d5e94fbb8ddbc9e52a08dd76191c246f
toandd-viosoft <toandd@viosoft.com>
2017-06-30 13:43:35 +0700

    Update 10-start-all.sh

--- scripts/one_vm_scripts/sut/10-start-all.sh
+++ scripts/one_vm_scripts/sut/10-start-all.sh
@@ -14,12 +14,13 @@ set -x
 source ../../env.sh
 source ./vm-args.sh
 ../../00-after-boot.sh
-[ -f /home/crucio-trusty-server.qcow2 ] && rm -rf /home/crucio-trusty-server.qcow2
+# Save time deploy server when run the second time
+# [ -f /home/crucio-trusty-server.qcow2 ] && rm -rf /home/crucio-trusty-server.qcow2
 ./del_tap_if_exists.sh
 ./90-stop-all.sh
 ./01-setup-env-before-testing.sh
 # Save time deploy server when run the second time
-./crucio-ubuntu-img-dpdk-modify ubuntu-server-cloudimg-dpdk-modify.sh
+# ./crucio-ubuntu-img-dpdk-modify ubuntu-server-cloudimg-dpdk-modify.sh
 ./20-start-ovsdb-server.sh
 ./30-start-ovs-vswitchd.sh
 ./40-setup-bridge.sh
