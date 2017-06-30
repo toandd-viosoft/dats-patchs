d72bfc0de3286c088e888d35b3cc1194f5561414
toandd-viosoft <toandd@viosoft.com>
2017-06-30 13:07:44 +0700

    Update 10-start-all.sh

--- scripts/one_vm_scripts/sut/10-start-all.sh
+++ scripts/one_vm_scripts/sut/10-start-all.sh
@@ -19,7 +19,7 @@ source ./vm-args.sh
 ./90-stop-all.sh
 ./01-setup-env-before-testing.sh
 # Save time deploy server when run the second time
-#./crucio-ubuntu-img-dpdk-modify ubuntu-server-cloudimg-dpdk-modify.sh
+./crucio-ubuntu-img-dpdk-modify ubuntu-server-cloudimg-dpdk-modify.sh
 ./20-start-ovsdb-server.sh
 ./30-start-ovs-vswitchd.sh
 ./40-setup-bridge.sh
