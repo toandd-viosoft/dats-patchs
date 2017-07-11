3f38af1c964dee289c268285dc287660f9645d52
toandd-viosoft <toandd@viosoft.com>
2017-07-11 11:30:27 +0700

    Update 06-create-VM_hard-disk_helper-scripts.sh

--- scripts/one_vm_scripts/sut/06-create-VM_hard-disk_helper-scripts.sh
+++ scripts/one_vm_scripts/sut/06-create-VM_hard-disk_helper-scripts.sh
@@ -23,9 +23,12 @@ git checkout a52953c4a92ecfcfdd06fc01fce42b57f998cb10
 cd -
 cp -a /opt/crucio/PROX/helper-scripts/start_vm.py _62-start-vm-helper.py
 chmod +x _62-start-vm-helper.py
+cur_path="$(pwd)"
 cd /opt/dats-patchs
 git checkout master
 patch -p1 </opt/dats-patchs/common-patchs/patch/prox/start-vm-helper.patch
+git checkout crucio-scripts
+cd $cur_path
 
 # Get management interface
 
